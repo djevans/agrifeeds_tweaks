f9b785a0121b3fe7b567be79ae6bf50d8d355618
David John Evans <david@djevans.info>
2014-12-30 07:58:31 +0000

    changed name to agrifeeds_tweaks

---
+++ views/templates/views-data-export-xml-body--ciard-ring-datasets.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file views-view-table.tpl.php
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $rows: An array of row items. Each row is an array of content
+ *   keyed by field ID.
+ * - $header: an array of headers(labels) for fields.
+ * - $themed_rows: a array of rows with themed fields.
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($themed_rows as $count => $row): ?>
+  <<?php print $item_node; ?>>
+  <?php foreach ($row as $field => $content): ?>
+<?php foreach ($content as $value): ?>
+  <<?php print $xml_tag[$field]; ?>><?php print $value; ?></<?php print $xml_tag[$field]; ?>>
+<?php endforeach; ?>
+  <?php endforeach; ?>
+</<?php print $item_node; ?>>
+<?php endforeach; ?>
